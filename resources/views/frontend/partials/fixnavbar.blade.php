2557b5d6d1be3e5b3efc508ed568a848be4b6295
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-10-24 15:05:33 +0100

    refactor welcome page

---
+++ resources/views/frontend/partials/fixnavbar.blade.php
@@ -0,0 +1,32 @@
+<ul class="icon-menu d-none d-lg-block revealator-slideup revealator-once revealator-delay1">
+    <li class="icon-box active">
+        <i class="fa fa-home"></i>
+        <a href="index.html">
+            <h2>Home</h2>
+        </a>
+    </li>
+    <li class="icon-box">
+        <i class="fa fa-user"></i>
+        <a href="about.html">
+            <h2>About</h2>
+        </a>
+    </li>
+    <li class="icon-box">
+        <i class="fa fa-briefcase"></i>
+        <a href="portfolio.html">
+            <h2>Portfolio</h2>
+        </a>
+    </li>
+    <li class="icon-box">
+        <i class="fa fa-envelope-open"></i>
+        <a href="contact.html">
+            <h2>Contact</h2>
+        </a>
+    </li>
+    <li class="icon-box">
+        <i class="fa fa-comments"></i>
+        <a href="blog.html">
+            <h2>Blog</h2>
+        </a>
+    </li>
+</ul>
