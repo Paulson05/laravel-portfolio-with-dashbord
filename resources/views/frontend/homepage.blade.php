392e579542e88b0288810490f515439b6cddbea3
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-12-23 15:55:40 +0100

    pics

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -7,7 +7,7 @@
         <div class="col-lg-4 bg position-fixed d-none d-lg-block"></div>
         <div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
             <div>
-                <img src="img/img-mobile.jpg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="my picture" />
+                <img src="img/myimg.jpg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="my picture" />
                 <h1 class="text-uppercase poppins-font">I'm Okofu onyebuchi t.<span>Back-end Developer</span></h1>
                 <p class="open-sans-font">I'm a Nigeria based Web developer & Backend‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
                 <a class="button" href="{{route('home.about')}}">
