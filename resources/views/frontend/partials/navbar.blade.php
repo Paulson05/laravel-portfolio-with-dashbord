7071816d2e1ace460c8cdb066b52320aad33fd3a
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-12-24 13:46:48 +0100

    active linke

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -4,10 +4,10 @@
     <span></span>
     <span></span>
     <ul class="list-unstyled" id="menu">
-        <li class="active"><a href="{{route('home.index')}}"><i class="fa fa-home"></i><span>Home</span></a></li>
-        <li><a href="{{route('home.about')}}"><i class="fa fa-user"></i><span>About</span></a></li>
-        <li><a href="{{route('home.portfolio')}}"><i class="fa fa-folder-open"></i><span>Portfolio</span></a></li>
-        <li><a href="{{route('home.contact')}}"><i class="fa fa-envelope-open"></i><span>Contact</span></a></li>
-        <li><a href="blog.html"><i class="fa fa-comments"></i><span>Blog</span></a></li>
+        <li class="{{ Route::currentRouteNamed('home.index') ? 'active' : '' }}"><a href="{{route('home.index')}}"><i class="fa fa-home"></i><span>Home</span></a></li>
+        <li class="{{ Route::currentRouteNamed('home.about') ? 'active' : '' }}" ><a href="{{route('home.about')}}"><i class="fa fa-user"></i><span>About</span></a></li>
+        <li class="{{ Route::currentRouteNamed('home.poerfolio') ? 'active' : '' }}" ><a href="{{route('home.portfolio')}}"><i class="fa fa-folder-open"></i><span>Portfolio</span></a></li>
+        <li class="{{ Route::currentRouteNamed('home.contact') ? 'active' : '' }}" ><a href="{{route('home.contact')}}"><i class="fa fa-envelope-open"></i><span>Contact</span></a></li>
+    
     </ul>
 </div>
