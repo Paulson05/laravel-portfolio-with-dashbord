169372e469921b9f1909d8d2232568d28ce6ab85
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-12-24 13:43:32 +0100

    home link

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -4,7 +4,7 @@
     <span></span>
     <span></span>
     <ul class="list-unstyled" id="menu">
-        <li class="active"><a href="{{route('home.about')}}"><i class="fa fa-home"></i><span>Home</span></a></li>
+        <li class="active"><a href="{{route('home.index')}}"><i class="fa fa-home"></i><span>Home</span></a></li>
         <li><a href="{{route('home.about')}}"><i class="fa fa-user"></i><span>About</span></a></li>
         <li><a href="{{route('home.portfolio')}}"><i class="fa fa-folder-open"></i><span>Portfolio</span></a></li>
         <li><a href="{{route('home.contact')}}"><i class="fa fa-envelope-open"></i><span>Contact</span></a></li>
