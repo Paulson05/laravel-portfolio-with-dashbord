7bca3e3913d698daa3fb940bced518bfb75a3a4b
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-12-25 15:43:15 +0100

    secure assset added

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -11,30 +11,30 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,600i,700" rel="stylesheet">
 
     <!-- Template CSS Files -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/preloader.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/circle.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/fm.revealator.jquery.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/preloader.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/circle.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/fm.revealator.jquery.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/style.css')}}" rel="stylesheet">
 
 
     <!-- CSS Skin File -->
-    <link href="{{asset('css/skins/yellow.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/skins/yellow.css')}}" rel="stylesheet">
 
     <!-- Live Style Switcher - demo only -->
-    <link rel="alternate stylesheet" type="text/css" title="blue" href="{{asset('css/skins/blue.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="green" href="{{asset('css/skins/green.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="yellow" href="{{asset('css/skins/yellow.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="blueviolet" href="{{asset('css/skins/blueviolet.css')}}')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="magenta" href="{{asset('css/skins/magenta.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="orange" href="{{asset('css/skins/orange.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="purple" href="{{asset('css/skins/purple.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="red" href="{{asset('css/skins/red.css')}}" />
-    <link rel="alternate stylesheet" type="text/css" title="yellowgreen" href="{{asset('css/skins/yellowgreen.css')}}" />
-    <link rel="stylesheet" type="text/css" href="{{asset('css/styleswitcher.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="blue" href="{{secure_asset('css/skins/blue.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="green" href="{{secure_asset('css/skins/green.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="yellow" href="{{secure_asset('css/skins/yellow.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="blueviolet" href="{{secure_asset('css/skins/blueviolet.css')}}')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="magenta" href="{{secure_asset('css/skins/magenta.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="orange" href="{{secure_asset('css/skins/orange.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="purple" href="{{secure_asset('css/skins/purple.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="red" href="{{secure_asset('css/skins/red.css')}}" />
+    <link rel="alternate stylesheet" type="text/css" title="yellowgreen" href="{{secure_asset('css/skins/yellowgreen.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('css/styleswitcher.css')}}" />
 
     <!-- Modernizr JS File -->
-    <script src="{{asset('js/modernizr.custom.js')}}"></script>
+    <script src="{{secure_asset('js/modernizr.custom.js')}}"></script>
 
 </head>
